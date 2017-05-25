42e592486d47d22435834869bab068d083a8da85
Ânderson Ignacio da Silva <anderson@aignacio.com>
2017-05-24 23:54:09 -0300

    Example adjusted

---
+++ README.md
@@ -0,0 +1,12 @@
+# Polly Jarvis
+## Brief
+Simple example to speak weather temperature receiving info from MQTT publishing using Amazon Polly.
+## Setup
+Read the post from the [blog](blog.aignacio.com) to see how it works exactly.
+### Run
+``` sh
+$ git clone ....
+$ npm install
+$ export CITY_POLLY="novo-hamburgo"
+$ node jarvis.js
+```
--- jarvis.js
+++ jarvis.js
@@ -1,36 +1,65 @@
 'use stric';
+let city = process.env.CITY_POLLY;
 let AWS = require('aws-sdk');
 let Stream = require('stream');
 let speaker = require('speaker');
 let weather = require("yahoo-weather");
+let mqtt = require('mqtt');
+let client = mqtt.connect('mqtt://test.mosquitto.org');
+
 const Polly = new AWS.Polly({
   signatureVersion: 'v4',
   region: 'us-east-1'
 });
-const Player = new speaker({
-  channels: 1,
-  bitDepth: 16,
-  sampleRate: 16000
+
+client.on('connect', function() {
+  console.log('City config:'+ city);
+  console.log('Connected to the MQTT Broker [test.mosquitto.org]!\nPub \'temp\' on /pollyjarvis/pt or /pollyjarvis/en');
+  client.subscribe('/pollyjarvis/#');
 });
 
-weather('novo-hamburgo').then(info => {
-  let inverno = info.item.condition.temp;
-  let params = {
-      'Text': '  Olá Anderson, a temperatura hoje é de '+inverno+' graus, use um agasalho',
-      'OutputFormat': 'pcm',
-      'VoiceId': 'Ricardo'
-  };
+client.on('message', function(topic, message) {
+  console.log('MQTT Pub: '+topic+' - '+message);
+  if (message.indexOf("temp") > -1) {
+    console.log("Requesting temperature...");
+    readWeatherProvis(topic.split('/')[2]);
+  }
+});
 
-  Polly.synthesizeSpeech(params, (err, data) => {
+function readWeatherProvis(language) {
+  weather(city).then(info => {
+    let weatherTemp = info.item.condition.temp;
+    let params;
+    if (language === 'pt') {
+      params = {
+        'Text': '  Olá amigo, a temperatura em '+city+' é de ' + weatherTemp + ' graus',
+        'OutputFormat': 'pcm',
+        'VoiceId': 'Ricardo'
+      };
+    } else {
+      params = {
+        'Text': '  Hi friend, the temperature on '+city+' is around '+weatherTemp + ' degrees Celsius',
+        'OutputFormat': 'pcm',
+        'VoiceId': 'Joey'
+      };
+    }
+
+    Polly.synthesizeSpeech(params, (err, data) => {
       if (err) {
-          console.log(err.code);
+        console.log(err.code);
       } else if (data) {
-          if (data.AudioStream instanceof Buffer) {
-              var bufferStream = new Stream.PassThrough();
-              bufferStream.end(data.AudioStream);
-              bufferStream.pipe(Player);
-          }
+        if (data.AudioStream instanceof Buffer) {
+          let Player = new speaker({
+            channels: 1,
+            bitDepth: 16,
+            sampleRate: 16000
+          });
+
+          let bufferStream = new Stream.PassThrough();
+          bufferStream.pipe(Player);
+          bufferStream.end(data.AudioStream);
+        }
       }
-  });
-}).catch(err => {
-});
+    });
+  }).catch(err => {});
+}
